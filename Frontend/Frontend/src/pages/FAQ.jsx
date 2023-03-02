156050150ebd4df74f0efd654f82620d010208c0
Grazi Firmino <grazi.firmino.15@gmail.com>
2023-03-02 00:34:24 -0300

    Update FAQ.jsx

--- Frontend/Frontend/src/pages/FAQ.jsx
+++ Frontend/Frontend/src/pages/FAQ.jsx
@@ -1,35 +1,13 @@
-import React from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
 
-function FAQ() {
+import FAQ from '../componentes/Faq';
+
+function FaqPage() {
   return (
-    <Accordion>
-      <Card>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey="0">
-            Como faço para doar itens?
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>
-            Para doar itens, basta preencher o formulário de doação em nossa página de doações.
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-      <Card>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey="1">
-            Qual o prazo de entrega das doações?
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="1">
-          <Card.Body>
-            O prazo de entrega das doações varia de acordo com a região de destino e a disponibilidade de transporte.
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <div>
+      <h1>Perguntas frequentes</h1>
+      <FAQ />
+    </div>
   );
 }
 
-export default FAQ;
+export default FaqPage;
